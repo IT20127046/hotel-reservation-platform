4569393970f2785d77653a1a195b12b46d190a05
IT20128036 <it20128036@my.sliit.lk>
2022-05-18 15:08:15 +0530

    fixed

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,16 @@ import ViewRoomDetails from './components/ViewRoomDetails';
 import EditRoomDetails from './components/EditRoomDetails';
 import ReserveRoom from './components/ReserveRoom';
 import CreateHotel from './components/CreateHotel';
-import ViewHotelDetails_HAdmin from './components/ViewHotelDetails_HAdmin';
 import EditHotelDetails from './components/EditHotelDetails';
+<<<<<<< HEAD
 import addReservedRoom from './components/addReservedRoom';
 import viewReservedRoom from './components/viewReservedRoom';
 import addReservedRoomTest from './components/addReservedRoomTest';
+=======
+import ViewHotelDetails_Customer from './components/ViewHotelDetails_Customer';
+import ViewHotelDetails_Admin from './components/ViewHotelDetails_Admin';
+import ViewHotelDetails_HAdmin from './components/ViewHotelDetails_HAdmin';
+>>>>>>> a88966934246b9a3bdfcc81c3f1ebd2117a0ae46
 
 
 export default class App extends Component {
@@ -18,19 +23,25 @@ export default class App extends Component {
       <BrowserRouter>
           <div>
           
-            <Route path="/" exact component={ViewRoomDetails}></Route>
+            <Route path="/rooms" exact component={ViewRoomDetails}></Route>
             <Route path="/room/add" component={CreateRoomDetails}></Route>
             <Route path="/edit/:id" component={EditRoomDetails}></Route>
             <Route path="/room/reserve" component={ReserveRoom}></Route>
 
+<<<<<<< HEAD
             <Route path="/addreserved/:id" component={addReservedRoom}></Route>
             <Route path="/viewreservedrooms" component={viewReservedRoom}></Route>
             <Route path="/addreservedtest/:id" component={addReservedRoomTest}></Route>
 
 
             <Route path="/view/hotels" exact component={ViewHotelDetails_HAdmin}></Route>
+=======
+            <Route path="/view/admin/hotels" exact component={ViewHotelDetails_Admin}></Route>
+            <Route path="/view/hadmin/hotels" exact component={ViewHotelDetails_HAdmin}></Route>
+>>>>>>> a88966934246b9a3bdfcc81c3f1ebd2117a0ae46
             <Route path="/add/hotel" exact component={CreateHotel}></Route>
             <Route path="/hotel/edit/:id" exact component={EditHotelDetails}></Route>
+            <Route path="/view/customer/hotels" exact component={ViewHotelDetails_Customer}></Route>
           </div>
       </BrowserRouter>
       
